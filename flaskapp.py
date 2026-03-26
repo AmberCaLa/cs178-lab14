02188be40a14a2b326916d47b6feca9f5bd37158
AmberCaLa <ambear06@gmail.com>
2026-03-26 15:03:02 -0500

    Artist Query 5

--- flaskapp.py
+++ flaskapp.py
@@ -82,13 +82,7 @@ def artist_query(artist_id):
     Fetches all tracks by a specific artist
     Route: /artistquery/<artist_id>
     """
-    rows = execute_query(""""
-        SELECT Track.Name
-        FROM Artist
-        JOIN Album USING (ArtistID)
-        JOIN Track USING (AlbumID)
-        WHERE ArtistId = %s
-        """,
+    rows = execute_query(""""SELECT Track.Name FROM Artist JOIN Album USING (ArtistID) JOIN Track USING (AlbumID) WHERE ArtistId = %s""",
         (artist_id, )
         )
     return display_html(rows)
